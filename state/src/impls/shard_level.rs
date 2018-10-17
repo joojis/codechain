5adb4a24f577d456a2742f76acae72426658cc1d
JinGyeong Jeong <jjg@kodebox.io>
2018-10-17 16:12:44 +0900

    Add timelock field to AssetTransferInput

--- state/src/impls/shard_level.rs
+++ state/src/impls/shard_level.rs
@@ -23,8 +23,7 @@ use ckey::Address;
 use cmerkle::{self, Result as TrieResult, TrieError, TrieFactory};
 use ctypes::invoice::TransactionInvoice;
 use ctypes::transaction::{
-    AssetMintOutput, AssetOutPoint, AssetTransferInput, AssetTransferOutput, Error as TransactionError, PartialHashing,
-    Transaction,
+    AssetMintOutput, AssetTransferInput, AssetTransferOutput, Error as TransactionError, PartialHashing, Transaction,
 };
 use ctypes::util::unexpected::Mismatch;
 use ctypes::{ShardId, WorldId};
@@ -321,7 +320,7 @@ impl<B: Backend + ShardBackend> ShardLevelState<B> {
     ) -> StateResult<()> {
         for (input, burn) in inputs.iter().map(|input| (input, false)).chain(burns.iter().map(|input| (input, true))) {
             let address = OwnedAssetAddress::new(input.prev_out.transaction_hash, input.prev_out.index, self.shard_id);
-            let script_result = self.check_and_run_input_script(input, transaction, &input.prev_out, burn)?;
+            let script_result = self.check_and_run_input_script(input, transaction, burn)?;
             match (script_result, burn) {
                 (ScriptResult::Unlocked, false) => {}
                 (ScriptResult::Burnt, true) => {}
@@ -389,7 +388,6 @@ impl<B: Backend + ShardBackend> ShardLevelState<B> {
         &self,
         input: &AssetTransferInput,
         transaction_hash: &PartialHashing,
-        cur: &AssetOutPoint,
         burn: bool,
     ) -> StateResult<ScriptResult> {
         let (address_hash, asset) = {
@@ -415,7 +413,7 @@ impl<B: Backend + ShardBackend> ShardLevelState<B> {
                 &lock_script,
                 transaction_hash,
                 VMConfig::default(),
-                cur,
+                input,
                 burn,
             ),
             // FIXME : Deliver full decode error
@@ -443,7 +441,7 @@ impl<B: Backend + ShardBackend> ShardLevelState<B> {
         let mut deleted_assets: Vec<(H256, _)> = Vec::with_capacity(inputs.len());
         for input in inputs.iter() {
             let (_, asset_address) = self.check_input_asset(input, sender)?;
-            let script_result = self.check_and_run_input_script(input, transaction, &input.prev_out, false)?;
+            let script_result = self.check_and_run_input_script(input, transaction, false)?;
 
             match script_result {
                 ScriptResult::Unlocked => {}
